81bf6a145d710b39a85f8232f9093c5b0b9c9bd7
Hussein-shahatet97 <hussein.sh1997@hotmail.com>
2020-04-22 23:25:02 +0300

    Add new files for database

--- server/database/config/db_build.js
+++ server/database/config/db_build.js
@@ -4,11 +4,11 @@ const connection = require("./db_connection");
 
 const sql = fs.readFileSync(path.join(__dirname, "db_build.sql")).toString();
 
-const createTables = callback => {
+const createTable = callback => {
   connection.query(sql, (err, res) => {
     if (err) throw err;
   });
 };
 
 createTable();
-module.exports = createTables;
+module.exports = createTable;
--- server/database/config/db_connection.js
+++ server/database/config/db_connection.js
@@ -1,10 +1,11 @@
 const { Pool } = require("pg");
 
-require("env2")("config.env");
+require("env2")("./config.env");
+
 const connectionString = process.env.DATABASE_URL;
 
 if (!connectionString) {
-  console.log(" unsuccessful connection");
+  throw new Error("set a DATABASE_URL env variable");
 }
 
 module.exports = new Pool({
