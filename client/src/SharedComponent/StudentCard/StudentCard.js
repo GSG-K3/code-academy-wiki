4ff270fb6c5eab929d697e3765f6d9e83fc44777
rahaf-96 <rahaf.hawamdeh96@gmail.com>
2020-04-23 17:13:54 +0300

    remove dummy data from student card component #66 #42

--- client/src/SharedComponent/StudentCard/StudentCard.js
+++ client/src/SharedComponent/StudentCard/StudentCard.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 import "./StudentCard.css";
-import adele from "../../images/adele.jpeg";
 
 class StudentCard extends Component {
   render() {
     return (
+      // we will get student image and name from database
       <div className="student-card">
-        <img className="student-img" src={adele} alt="student" />
-        <span className="student-name"> Adele</span>
+        <img className="student-img" src={""} alt="student profile" />
+        <span className="student-name"></span>
       </div>
     );
   }
