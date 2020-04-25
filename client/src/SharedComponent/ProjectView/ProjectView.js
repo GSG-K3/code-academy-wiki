6e9b72c1acf01931c46cc2e9f7eb4f3d9f162298
kholoudfann <141060@ppu.edu.ps>
2020-04-26 00:47:18 +0300

    create projectview share component #45

---
+++ client/src/SharedComponent/ProjectView/ProjectView.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './ProjectView.css';
+class ProjectView extends Component {
+    render() {
+      return (
+        // we will get student image and name from database
+        <div className="project-view">
+         <div className ="project-name"> Project View</div>
+        </div>
+      );
+    }
+  }
+  
+  export default ProjectView;
+  
