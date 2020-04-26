9fc0e5b8f8b1832c242259bda452de82f3197e1f
rahaf-96 <rahaf.hawamdeh96@gmail.com>
2020-04-26 15:00:36 +0200

    handle null or empty projects on slideshow #74 #67 #44

--- client/src/SharedComponent/Slideshow/Slideshow.js
+++ client/src/SharedComponent/Slideshow/Slideshow.js
@@ -3,10 +3,8 @@ import "./Slideshow.css";
 import facebook from "../../images/facebook.jpeg";
 import google from "../../images/google.png";
 import website from "../../images/website.jpg";
-
 import { Slide } from "react-slideshow-image";
-const slideImages = [facebook, google, website, google, google, website];
-
+const allProjects = [facebook, website, "", google, google, facebook, google, website];
 const properties = {
   duration: 5000000000000000,
   transitionDuration: 500,
@@ -16,29 +14,41 @@ const properties = {
 };
 
 class Slideshow extends Component {
-
-state = {
-  projects : []
-}
-componentDidMount = () => {
-  let items = [];
-  for (let i = 0; i < slideImages.length; i += 3) {
-    items.push(<div key ={Date.now()}>
-      <img className='website' src={slideImages[i]} alt =""/>
-      <img className='website' src={slideImages[i+1]} alt =""/>
-      <img className='website' src={slideImages[i+2]} alt =""/>
-    </div>)
+  state = {
+    projects: [],
   }
-  this.setState({projects: items})
-}
- 
-  render() {
+  componentDidMount = () => {
+    let projectsSlide = [];
+    for (let i = 0; i < allProjects.length; i += 3) {
+      projectsSlide.push(<div className="one-slide" key={Date.now()}>
+        {!allProjects[i]
+          ? <div></div>
+          : <img className='website' src={allProjects[i]} alt="" />
+        }
+        {!allProjects[i + 1]
+          ? <div></div>
+          : <img className='website' src={allProjects[i + 1]} alt="" />
+        }
+        {!allProjects[i + 2]
+          ? <div></div>
+          : <img className='website' src={allProjects[i + 2]} alt="" />
+        }
 
+      </div>)
+    }
+    this.setState(prevState => ({
+      projects: prevState.projects.concat(projectsSlide)
+    }))
+  }
+
+  render() {
     return (
-      <div className="slideshow">
-        <Slide {...properties}>
-          {this.state.projects}
-        </Slide>
+      <div>
+        <div className="projects-slide">
+          <Slide {...properties}>
+            {this.state.projects}
+          </Slide>
+        </div>
       </div>
     );
   }
