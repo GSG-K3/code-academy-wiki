ca07ec28c0baca96f187ad258a1771e7a21676fb
Hussein-shahatet97 <hussein.sh1997@hotmail.com>
2020-04-28 00:16:02 +0300

    remove hardcode cards from cohort card page

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Footer from './SharedComponents/Footer/Footer';
 import NavBar from './SharedComponents/NavBar/NavBar'
+import CohortCard from "./SharedComponents/CohortCard/CohortCard";
 function App() {
   return (
     <div>
-      <NavBar />
-      <Footer/>
+      {/* <NavBar />
+      <Footer/> */}
+      <CohortCard />
     </div>
   );
 }
