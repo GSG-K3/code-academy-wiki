1b863ff357344f99d4269be896c6d7daea58d3d5
rahaf-96 <rahaf.hawamdeh96@gmail.com>
2020-04-28 00:42:15 +0200

    create Home page and style it #20 #46 #78

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
-import Footer from './SharedComponents/Footer/Footer';
-import NavBar from './SharedComponents/NavBar/NavBar'
+import Footer from './SharedComponents/Footer';
+import NavBar from './SharedComponents/NavBar/NavBar';
+import Home from './LayoutComponents/Home';
 function App() {
   return (
     <div>
       <NavBar />
-      <Footer/>
+      <Home />
+      <Footer />
     </div>
   );
 }
