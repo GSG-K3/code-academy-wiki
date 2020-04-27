f220dcbfe9b02366c80f8a5eadf8bde7ddd5e5e3
kholoudfann <141060@ppu.edu.ps>
2020-04-27 21:05:44 +0300

    delete comments #94

--- client/src/SharedComponents/Footer/Footer.js
+++ client/src/SharedComponents/Footer/Footer.js
@@ -7,17 +7,30 @@ import youtubeicon from '../../images/youtubeicon.svg';
 class Footer extends Component {
   render() {
     return (
-      // we will get student image and name from database
       <div className="main-footer">
         <div className="container">
           <div className="footer-text"> © 2020 Gaza Sky Geeks.</div>
           <div className="footer-icons">
-          <a href="https://www.facebook.com/GazaSkyGeeks" target="_blank">  <img className="icon-img" src={facebookicon} alt="facebook icon" /> </a>
-           
-        <a href="https://www.instagram.com/gazaskygeeks" target="_blank"  ><img  className="icon-img" src={instagramicon} alt=" instagrm icon" /> 
-         </a>
-            <a href="https://twitter.com/gazaskygeeks" target="_blank"> <img className="icon-img" src={twittericon} alt=" twitter icon" /> </a>
-         <a href="https://www.youtube.com/gazaskygeeks" target="_blank">  <img className="icon-img" src={youtubeicon} alt="youtube icon" /></a> 
+            <a href="https://www.facebook.com/GazaSkyGeeks" target="_blank">
+              <img
+                className="icon-img"
+                src={facebookicon}
+                alt="facebook icon"
+              />
+            </a>
+            <a href="https://www.instagram.com/gazaskygeeks" target="_blank">
+              <img
+                className="icon-img"
+                src={instagramicon}
+                alt=" instagrm icon"
+              />
+            </a>
+            <a href="https://twitter.com/gazaskygeeks" target="_blank">
+              <img className="icon-img" src={twittericon} alt=" twitter icon" />
+            </a>
+            <a href="https://www.youtube.com/gazaskygeeks" target="_blank">
+              <img className="icon-img" src={youtubeicon} alt="youtube icon" />
+            </a>
           </div>
         </div>
       </div>
