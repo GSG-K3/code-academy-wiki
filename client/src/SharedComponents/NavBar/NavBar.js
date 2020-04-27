e112c3b169e746011d27577c0fd63b0fcb1599f5
bayan-404 <bayan.jubah@gmail.com>
2020-04-28 01:08:34 +0300

    fix style

--- client/src/SharedComponents/NavBar/NavBar.js
+++ client/src/SharedComponents/NavBar/NavBar.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./NavBar.css";
-import logo from "../../images/logo.svg";
+import logo from "../../images/logo.png";
 
 function NavBar(props) {
   return (
