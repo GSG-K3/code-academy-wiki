41be56a10406a4476698ae2ae5360bedb566e0b3
bayan-404 <bayan.jubah@gmail.com>
2020-05-03 20:47:16 +0300

    get the data using axious #116

--- client/src/LayoutComponents/Cohorts/Cohorts.js
+++ client/src/LayoutComponents/Cohorts/Cohorts.js
@@ -1,15 +1,31 @@
 import React, { Component } from "react";
 import "./Cohorts.css";
 import CohortCard from "../../SharedComponents/CohortCard";
-import NavBar from "../../SharedComponents/NavBar";
 import Slideshow from "../../SharedComponents/Slideshow";
-import Footer from "../../SharedComponents/Footer";
+import axios from "axios";
 
 class Cohorts extends Component {
+  componentDidMount() {
+    const cityName = this.props.match.params.city;
+    axios
+      .get(`/cohorts/${cityName}`)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
   render() {
+    const cityName = this.props.match.params.city;
+    // console.log(
+    //   this.props.history.location.state.data,
+    //   "this is cohorts page test "
+    // );
+
     return (
       <div>
-        <h1 className="title">Gaza Cohorts</h1>
+        <h1 className="title">{cityName} Cohorts</h1>
         <div className="cohort">
           <div className="coh_card">
             <CohortCard />
