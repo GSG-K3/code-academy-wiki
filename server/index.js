51f3acc88d5695eae5e4a8e8ae7fe7d585a3c2b2
bayan-404 <bayan.jubah@gmail.com>
2020-05-03 20:57:05 +0300

    add server port 4000

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 
-app.set("port", process.env.PORT || 3000);
+app.set("port", process.env.PORT || 4000);
 
 app.listen(app.get("port"), () => {
   console.log(`App is live on http://localhost:${app.get("port")}`);
