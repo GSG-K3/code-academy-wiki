6f1dfb2401c6736f797477eb3e7f18ce03888601
kholoudfann <141060@ppu.edu.ps>
2020-05-04 22:50:12 +0300

    make the projectDeatails page with react and css and path #23 #49 #81

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,17 @@ import './App.css';
 import Footer from './SharedComponents/Footer';
 import NavBar from './SharedComponents/NavBar/NavBar';
 import Home from './LayoutComponents/Home';
-import { Route, BrowserRouter as Router } from 'react-router-dom'
+import { Route, BrowserRouter as Router } from 'react-router-dom';
+import ProjectDetails from './LayoutComponents/ProjectDetails/ProjectDetails';
 
 function App() {
   return (
     <Router>
       <NavBar />
-      <Route exact path="/" component={Home} />      
+      <Route exact path='/' component={Home} />
+      <Route exact path='/project/:id' component={ProjectDetails} />
       <Footer />
     </Router>
-
   );
 }
 export default App;
---
+++ client/src/LayoutComponents/ProjectDetails/ProjectDetails.js
@@ -0,0 +1,103 @@
+import React, { Component } from 'react';
+import './ProjectDetails.css';
+import github from '../../images/github.svg';
+import pc from '../../images/pc.svg';
+import ProjectView from '../../SharedComponents/ProjectView';
+import StudentCard from '../../SharedComponents/StudentCard';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+class ProjectDetails extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      projects: [],
+    };
+  }
+  componentDidMount() {
+    const { id } = this.props;
+    // id coming from cohorts and studentProfile pages
+    axios
+      .get(`api/project/${id}`)
+      .then((res) => {
+        const resultArray = res.data.map((e) => e);
+        this.setState({
+          projects: resultArray,
+        });
+      })
+      .catch((error) => error.response);
+  }
+  render() {
+    const { projects } = this.state;
+
+    return (
+      <div className='projectdetails-container'>
+        {!projects.length ? (
+          <div className='loading-container'>
+            <h1 className='loading'> </h1>
+          </div>
+        ) : (
+          <div>
+            <section className='projects-details'>
+              <div className='projects-info'>
+                <h2 className='project-title'>{projects[0].title}</h2>
+                <p className='project-desc'>
+                  {projects[0].description}
+                </p>
+                <div className='icon-images'>
+                  <div className='github-name'>
+                    <a
+                      href={projects[0].github}
+                      target='_blank'
+                      rel='noopener noreferrer'
+                    >
+                      <img
+                        className='github-img'
+                        src={github}
+                        alt='github icon'
+                      />
+                    </a>
+                    GitHub
+                  </div>
+                  <div className='heroku-name'>
+                    <a
+                      href={projects[0].heroku}
+                      target='_blank'
+                      rel='noopener noreferrer'
+                    >
+                      <img className='pc-img' src={pc} alt='pc icon' />
+                    </a>
+                    LiveView
+                  </div>
+                </div>
+              </div>
+              <div>
+                <ProjectView
+                  projectDetailsStyle={true}
+                  projectImg={projects[0].image}
+                />
+              </div>
+            </section>
+
+            <section className='team-members'>
+              <div className='title'> Team Members</div>
+              <div className='students-info'>
+                {projects.map((student) => {
+                  return (
+                    <Link to={'/student/' + student.id} className='text-link'>
+                      <StudentCard
+                        studentImg={student.img}
+                        studentname={student.name}
+                      />
+                    </Link>
+                  );
+                })}
+              </div>
+            </section>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default ProjectDetails;
