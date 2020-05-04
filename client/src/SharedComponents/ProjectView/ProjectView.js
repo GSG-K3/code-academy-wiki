024307f634d9ddf3b3200e12d43d3661945b7b96
kholoudfann <141060@ppu.edu.ps>
2020-05-04 23:13:11 +0300

    make a specific style for projectview component in projectDetails page #121

--- client/src/SharedComponents/ProjectView/ProjectView.js
+++ client/src/SharedComponents/ProjectView/ProjectView.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './ProjectView.css';
 
-const ProjectView = ({ projectImg }) => {
+const ProjectView = ({ projectImg, projectDetailsStyle }) => {
   return (
-    <img className='project-view' src={projectImg} alt="" />
-  )
-}
+    <div>
+      {projectDetailsStyle ? (
+        <img className='project-imgview' src={projectImg} alt='' />
+      ) : (
+        <img className='project-view' src={projectImg} alt='' />
+      )}
+    </div>
+  );
+};
 
 export default ProjectView;
