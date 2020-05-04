658e85bbab2fa3ac10ad6627b63a7987f85e8cbb
bayan-404 <bayan.jubah@gmail.com>
2020-05-05 00:30:24 +0300

    get the data

--- client/src/LayoutComponents/Home/Home.js
+++ client/src/LayoutComponents/Home/Home.js
@@ -1,52 +1,55 @@
-import React, { Component } from "react";
-import computer from "../../images/home-illustrations/computer.svg";
-import girl from "../../images/home-illustrations/girl.png";
-import boy from "../../images/home-illustrations/boy.png";
-import technologies from "../../images/home-illustrations/technologies.png";
-import "./Home.css";
-import { Link } from "react-router-dom";
+import React, { Component } from 'react';
+import computer from '../../images/home-illustrations/computer.svg';
+import girl from '../../images/home-illustrations/girl.png';
+import boy from '../../images/home-illustrations/boy.png';
+import technologies from '../../images/home-illustrations/technologies.png';
+import './Home.css';
+import { Link } from 'react-router-dom';
 class Home extends Component {
-  state = {
+  constructor(props) {
+    super(props);
+    console.log(props, "these are the props")
+  this.state = {
     city: null,
   };
-
-  render() {
-    const { city } = this.state;
-    const { history } = this.props;
-    console.log(city, "please work!");
+}
+render() {
+  const { city } = this.state;
+  const { history } = this.props;
+    console.log(city, 'please work!');
     return (
       <div>
-        <section className="home-container">
-          <div className="home-intro">
-            <p className="page-title">
+        <section className='home-container'>
+          <div className='home-intro'>
+            <p className='page-title'>
               <b>Gaza Sky Geeks</b>
               <br /> Code Academy Wiki
             </p>
-            <p className="description">
+            <p className='description'>
               The GSG Code Academy is Palestine’s first immersive, 6-month
               career accelerating web development bootcamp.
             </p>
             <a
-              href="https://gazaskygeeks.com/code/"
-              target="_blank"
-              rel="noopener noreferrer"
+              href='https://gazaskygeeks.com/code/'
+              target='_blank'
+              rel='noopener noreferrer'
             >
-              <button className="join-btn">Join Us</button>
+              <button className='join-btn'>Join Us</button>
             </a>
           </div>
-          <img src={computer} alt="" />
+          <img src={computer} alt='' />
         </section>
 
-        <section className="cohorts-container">
-          <span className="cohorts-title">Code Academy Cohorts</span>
-          <div className="cohorts">
-            <img src={girl} alt="" />
-            <div className="cohorts-btns">
+        <section className='cohorts-container'>
+          <span className='cohorts-title'>Code Academy Cohorts</span>
+          <div className='cohorts'>
+            <img src={girl} alt='' />
+            <div className='cohorts-btns'>
               <Link>
                 <button
-                  className="cohort-btn"
+                  className='cohort-btn'
                   onClick={() => {
-                    this.setState({ city: "gaza" });
+                    this.setState({ city: 'gaza' });
                   }}
                 >
                   Gaza
@@ -54,27 +57,27 @@ class Home extends Component {
               </Link>
               <Link>
                 <button
-                  className="cohort-btn"
+                  className='cohort-btn'
                   onClick={() => {
-                    this.setState({ city: "Kalel" });
+                    this.setState({ city: 'Kalel' });
                   }}
                 >
                   Khalil
                 </button>
               </Link>
-              {city === "gaza" || city === "Kalel"
+              {city === 'gaza' || city === 'Kalel'
                 ? history.push(`/cohorts/${city}`, { data: city })
                 : console.log(city)}
             </div>
-            <img src={boy} alt="" />
+            <img src={boy} alt='' />
           </div>
         </section>
 
-        <section className="technologies">
-          <span className="technologies-title">
+        <section className='technologies'>
+          <span className='technologies-title'>
             Technologies Used in Our Academy
           </span>
-          <img src={technologies} alt="" />
+          <img src={technologies} alt='' />
         </section>
       </div>
     );
