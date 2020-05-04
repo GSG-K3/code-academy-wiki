229819a8476e8e02769314856498eda7236ed001
bayan-404 <bayan.jubah@gmail.com>
2020-05-05 00:32:49 +0300

    display the data #116

--- client/src/LayoutComponents/Cohorts/Cohorts.js
+++ client/src/LayoutComponents/Cohorts/Cohorts.js
@@ -1,16 +1,24 @@
-import React, { Component } from "react";
-import "./Cohorts.css";
-import CohortCard from "../../SharedComponents/CohortCard";
-import Slideshow from "../../SharedComponents/Slideshow";
-import axios from "axios";
+import React, { Component } from 'react';
+import './Cohorts.css';
+import CohortCard from '../../SharedComponents/CohortCard';
+import Slideshow from '../../SharedComponents/Slideshow';
+import axios from 'axios';
 
 class Cohorts extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cohortInfo: [],
+    };
+  }
   componentDidMount() {
     const cityName = this.props.match.params.city;
     axios
       .get(`/cohorts/${cityName}`)
-      .then((response) => {
-        console.log(response.data);
+      .then((res) => {
+        const resData = res.data.map((e) => e);
+        this.setState({ cohortInfo: resData });
+        console.log(this.state);
       })
       .catch((error) => {
         console.log(error);
@@ -18,20 +26,20 @@ class Cohorts extends Component {
   }
   render() {
     const cityName = this.props.match.params.city;
-    // console.log(
-    //   this.props.history.location.state.data,
-    //   "this is cohorts page test "
-    // );
-
+    const { cohortInfo } = this.state;
+        
     return (
+      
       <div>
-        <h1 className="title">{cityName} Cohorts</h1>
-        <div className="cohort">
-          <div className="coh_card">
-            <CohortCard />
+      {!cohortInfo.length? <h1>loading</h1>: <div>
+        <h1 className='title'>{cityName} Cohorts</h1>
+        <div className='cohort'>
+          <div className='coh_card'>
+          {cohortInfo.map((e) =>(<CohortCard name={e.name} /> ))}
           </div>
           <Slideshow />
         </div>
+      </div> }
       </div>
     );
   }
