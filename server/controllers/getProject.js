7a18045269a17ecbc435dea4ec165e5a7fb7a144
kholoudfann <141060@ppu.edu.ps>
2020-05-06 02:10:55 +0300

    edit on path #81 #49 #23

--- server/controllers/getProject.js
+++ server/controllers/getProject.js
@@ -1,9 +1,8 @@
 const getProjectById = require('../database/queries/getProject');
 
-  const getProject = (req ,res) =>{
+  exports.getProject = (req ,res) =>{
      getProjectById(req.params.id)
      .then(data => res.json(data))
      .catch(() => res.status(500).json({ err: 'Error for get projects' })); 
 
  }
- module.exports =  getProject; 
