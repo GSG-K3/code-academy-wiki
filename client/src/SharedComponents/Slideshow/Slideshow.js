26955900c7a99eece0b3b559fda47d1927cf6acc
rahaf-96 <rahaf.hawamdeh96@gmail.com>
2020-05-05 05:43:20 +0200

    edit slideshow component by removing logic from componentdidmount and put it in render #109

--- client/src/SharedComponents/Slideshow/Slideshow.js
+++ client/src/SharedComponents/Slideshow/Slideshow.js
@@ -1,94 +1,104 @@
-import React, { Component } from "react";
-import StudentCard from '../StudentCard'
-import "./Slideshow.css";
-import Slider from "react-slick";
-import ProjectView from '../ProjectView'
-
+import React, { Component } from 'react';
+import StudentCard from '../StudentCard';
+import Slider from 'react-slick';
+import ProjectView from '../ProjectView';
+import './Slideshow.css';
 
 class Slideshow extends Component {
-  state = {
-    projectSlide: [],
-    studentSlide: [],
-  }
-
-  componentDidMount() {
-    const { projects, students } = this.props;
-
-    if (projects) {
-      const projectSlide = projects.map(project => {
-        return <ProjectView
-          projectImg={project.image}
-        />
-      })
-      this.setState({ projectSlide })
-
-    }
-    if (students) {
-      const studentSlide = students.map(student => {
-        return <StudentCard
-          studentImg={student.img}
-          studentname={student.name} />
-      })
-      this.setState({ studentSlide })
-
-    }
-  }
-
   render() {
-    const { projectSlide, studentSlide } = this.state;
+    const { projects, students, mentors } = this.props;
+
+    const projectSlide = projects.map((project) => {
+      return <ProjectView projectImg={project.project_image} />;
+    });
+    const studentSlide = students.map((student) => {
+      return (
+        <StudentCard studentImg={student.img} studentname={student.name} />
+      );
+    });
+    const mentorSlide = mentors.map((mentor) => {
+      return (
+        <StudentCard
+          studentImg={mentor.mentor_img}
+          studentname={mentor.mentor_name}
+        />
+      );
+    });
+
     return (
       <div className='slide-container'>
-        {!projectSlide.length && !studentSlide.length
-          ? <h1>Loading ....</h1>
-          : <Slider {...SlideSettings()}>   {projectSlide.length ? projectSlide : studentSlide}   </Slider>
-
-        }
+        {!projectSlide.length && !studentSlide.length && !mentorSlide.length ? (
+          <h1 className='loading'>Loading ....</h1>
+        ) : (
+          <Slider {...SlideSettings}>
+            {projectSlide.length ? (
+              projectSlide
+            ) : studentSlide.length ? (
+              studentSlide
+            ) : mentorSlide.length ? (
+              mentorSlide
+            ) : (
+              <h1>Something wrong happend...</h1>
+            )}
+          </Slider>
+        )}
       </div>
-    )
+    );
   }
 }
 export default Slideshow;
+
+// create default props as an arrays to avoid mapping on null values
+Slideshow.defaultProps = {
+  projects: [],
+  students: [],
+  mentors: [],
+};
+
+// to give slideshow customs arrows
 function Arrow(props) {
   const { className, style, onClick } = props;
   return (
     <div
       className={className}
-      style={{ ...style, display: "block", background: "grey", borderRadius: '50%' }}
+      style={{
+        ...style,
+        display: 'block',
+        background: 'grey',
+        borderRadius: '50%',
+      }}
       onClick={onClick}
     />
   );
 }
 
-function SlideSettings() {
-  const settings = {
-    dots: true,
-    infinite: false,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    initialSlide: 0,
-    nextArrow: <Arrow />,
-    prevArrow: <Arrow />,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          infinite: true,
-          dots: true,
-        }
+// slideshow settings that makes it responsive
+const SlideSettings = {
+  dots: true,
+  infinite: false,
+  speed: 500,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  initialSlide: 0,
+  nextArrow: <Arrow />,
+  prevArrow: <Arrow />,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+        dots: true,
       },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          initialSlide: 1,
-        }
-      }
-    ]
-  };
-  return settings;
-}
-
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        initialSlide: 1,
+      },
+    },
+  ],
+};
