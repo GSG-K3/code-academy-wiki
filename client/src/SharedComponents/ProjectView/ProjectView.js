5eb8fbcaf49050ceb4f1adf7208f826b75211bdf
kholoudfann <141060@ppu.edu.ps>
2020-05-06 02:12:02 +0300

    reduce some dublication #81 #49 #23

--- client/src/SharedComponents/ProjectView/ProjectView.js
+++ client/src/SharedComponents/ProjectView/ProjectView.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import './ProjectView.css';
 
-const ProjectView = ({ projectImg, projectDetailsStyle }) => {
-  return (
-    <div>
-      {projectDetailsStyle ? (
-        <img className='project-imgview' src={projectImg} alt='' />
-      ) : (
-        <img className='project-view' src={projectImg} alt='' />
-      )}
-    </div>
-  );
-};
+// const ProjectView = ({ projectImg, projectDetailsStyle }) => {
+//   return (
+//     <div>
+//       {projectDetailsStyle ? (
+//         <img className='project-imgview' src={projectImg} alt='' />
+//       ) : (
+//         <img className='project-view' src={projectImg} alt='' />
+//       )}
+//     </div>
+//   );
+// };
 
+const ProjectView = ({ projectImg, projectDetailsStyle }) => {
+  const className = projectDetailsStyle ? 'project-imgview' : 'project-view';
+  return( <img className={className} src={projectImg} alt='' />
+)}
 export default ProjectView;
