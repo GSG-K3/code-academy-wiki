9f569a9b6efc46055bf638b09477baf39b84123c
rahaf-96 <rahaf.hawamdeh96@gmail.com>
2020-05-05 05:37:26 +0200

    add link to projects that when the user click on one of them, it will be redirected to a project details page

--- client/src/LayoutComponents/StudentProfile/StudentProfile.js
+++ client/src/LayoutComponents/StudentProfile/StudentProfile.js
@@ -1,60 +1,78 @@
-import React, { Component } from "react";
-import axios from "axios";
-import email from '../../images/contact-icons/email.svg'
-import github from '../../images/contact-icons/github.svg'
-import phone from '../../images/contact-icons/phone.svg'
-import house from '../../images/contact-icons/house.svg'
-import Project from '../../SharedComponents/ProjectView'
-import "./StudentProfile.css";
+import React, { Component } from 'react';
+import axios from 'axios';
+import email from '../../images/contact-icons/email.svg';
+import github from '../../images/contact-icons/github.svg';
+import phone from '../../images/contact-icons/phone.svg';
+import house from '../../images/contact-icons/house.svg';
+import Project from '../../SharedComponents/ProjectView';
+import {Link} from 'react-router-dom'
+import './StudentProfile.css';
 
 class StudentProfile extends Component {
-    state = {
-        studentInfo: null,
-        studentProjects: []
-    }
-    componentDidMount() {
-        // student information will be fetch from database based on student id that passed to this component as a props
-        const { id } = this.props;
-        axios
-            .get(`/api/students/${id}`)
-            .then(res => {
-                const studentProjects = res.data.map(student => student.image)
-                this.setState({
-                    studentInfo: res.data[0],
-                    studentProjects
-                })
-            })
-            .catch(err => err);
-    }
+  state = {
+    studentInfo: null,
+    studentProjects: [],
+  };
+  componentDidMount() {
+    // student information will be fetch from database based on student id that passed to this component as a props
+    const { id } = this.props;
+    axios
+      .get(`/api/students/${id}`)
+      .then((res) => {
+        this.setState({
+          studentInfo: res.data[0],
+          studentProjects: res.data
+        });
+      })
+      .catch((err) => err);
+  }
 
-    render() {
-        const { studentInfo, studentProjects } = this.state;
-        const projects = studentProjects.map((project, i) => {
-            return <Project key={i} projectImg={project} />
-        }
-        )
-        return (
-            <div>
-                {!studentInfo ? (<div className='loading'>Loading...</div>)
-                    : (<div className='profile-container'>
-                        <div className="student-card">
-                            <img className='student-img' src={studentInfo.img} alt='' />
-                            <span className='student-name'>{studentInfo.name} </span>
-                            <span className='student-role'> {studentInfo.about} </span>
-                            <div className="contact-section">
-                                <div className='contact-info'><img className='contact-icon' src={email} alt='' /><span>{studentInfo.email}</span></div>
-                                <div className='contact-info'><img className='contact-icon' src={github} alt='' /><span>{studentInfo.github}</span></div>
-                                <div className='contact-info'><img className='contact-icon' src={phone} alt='' /><span>{studentInfo.phone_no}</span></div>
-                                <div className='contact-info'><img className='contact-icon' src={house} alt='' /><span>{studentInfo.adress}</span></div>
-                            </div>
-                        </div>
-                        <div className="student-projects">
-                            <p className="projects-title">{studentInfo.name} Projects</p>
-                            {projects}
-                        </div>
-                    </div>)
-                }
-            </div>);
-    }
+  render() {
+    const { studentInfo, studentProjects } = this.state;
+    const projects = studentProjects.map((project, i) => {
+      return(
+        <Link to={'/project/' + project.project_id} key={i}>
+         <Project key={project.project_id} projectImg={project.project_image} />
+         </Link>
+)
+    });
+    return (
+      <div>
+        {!studentInfo ? (
+          <div className='loading'>Loading...</div>
+        ) : (
+          <div className='profile-container'>
+            <div className='student-card'>
+              <img className='student-img' src={studentInfo.img} alt='' />
+              <span className='student-card-name'>{studentInfo.name} </span>
+              <span className='student-role'> {studentInfo.about} </span>
+              <div className='contact-section'>
+                <div className='contact-info'>
+                  <img className='contact-icon' src={email} alt='' />
+                  <span>{studentInfo.email}</span>
+                </div>
+                <div className='contact-info'>
+                  <img className='contact-icon' src={github} alt='' />
+                  <span>{studentInfo.github}</span>
+                </div>
+                <div className='contact-info'>
+                  <img className='contact-icon' src={phone} alt='' />
+                  <span>{studentInfo.phone_no}</span>
+                </div>
+                <div className='contact-info'>
+                  <img className='contact-icon' src={house} alt='' />
+                  <span>{studentInfo.adress}</span>
+                </div>
+              </div>
+            </div>
+            <div className='student-projects'>
+              <p className='projects-title'>{studentInfo.name} Projects</p>
+              {projects}
+            </div>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 export default StudentProfile;
