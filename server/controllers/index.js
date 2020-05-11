ee2f9c8dcfc23f5c6b6a65e9d86b0aba09751713
Hussein-shahatet97 <hussein.sh1997@hotmail.com>
2020-05-12 01:59:21 +0300

    add path to getCohortProjects

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -6,6 +6,7 @@ const { getProject } = require('./getProject');
 const { getAllStudents } = require('./allStudents');
 const { getCohortstudent } = require('./getCohortstudents');
 const { getMentorData } = require('./getMentorData');
+const { getCohortProjects } = require('./getCohortProjects');
 
 router.get('/api/cohortinfo/:cohortID', getCohortData);
 router.get('/api/students', getAllStudents);
@@ -13,4 +14,5 @@ router.get('/api/students/:id', getStudent);
 router.get('/api/project/:id', getProject);
 router.get('/api/cohortstd/:cohortID', getCohortstudent);
 router.get('/api/cohortMentor/:cohortID', getMentorData);
+router.get('/api/cohortProjects/:cohortID', getCohortProjects);
 module.exports = router;
