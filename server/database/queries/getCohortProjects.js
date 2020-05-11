5c45407e4842fa9849f7ecb9d54a9e973d8f7ca4
Hussein-shahatet97 <hussein.sh1997@hotmail.com>
2020-05-12 02:03:59 +0300

    define new query that get projects id's,and images

---
+++ server/database/queries/getCohortProjects.js
@@ -0,0 +1,13 @@
+const db_connection = require('../config/db_connection');
+const getCohortProjects = (cohortID) => {
+  const sql = {
+    text:
+      'select p.id project_id,p.image project_image from projects p,cohorts c where c.id=p.cohort_id AND c.id=$1;',
+    values: [cohortID],
+  };
+  return db_connection
+    .query(sql)
+    .then((result) => result.rows)
+    .catch((error) => error);
+};
+module.exports = getCohortProjects;
