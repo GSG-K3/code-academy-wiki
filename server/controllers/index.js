5c629c31408af264fa6aea85deb31619470c03ad
Hussein-shahatet97 <hussein.sh1997@hotmail.com>
2020-05-11 04:30:05 +0300

    define new urls that show result of query in the server and to use them at the client side in axios

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -4,9 +4,13 @@ const { getCohortData } = require('./getCohortData');
 const { getStudent } = require('./student');
 const { getProject } = require('./getProject');
 const { getAllStudents } = require('./allStudents');
+const { getCohortstudent } = require('./getCohortstudents');
+const { getMentorData } = require('./getMentorData');
 
 router.get('/api/cohortinfo/:cohortID', getCohortData);
 router.get('/api/students', getAllStudents);
 router.get('/api/students/:id', getStudent);
 router.get('/api/project/:id', getProject);
+router.get('/api/cohortstd/:cohortID', getCohortstudent);
+router.get('/api/cohortMentor/:cohortID', getMentorData);
 module.exports = router;
