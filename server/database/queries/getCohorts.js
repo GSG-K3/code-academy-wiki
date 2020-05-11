c220c5a5faac6f0574fd091bdc7a99f4cc26f8f6
bayan-404 <bayan.jubah@gmail.com>
2020-05-11 03:00:52 +0300

    re-write the quere

--- server/database/queries/getCohorts.js
+++ server/database/queries/getCohorts.js
@@ -1,17 +1,17 @@
-const dbconnection = require("../config/db_connection");
-console.log("hi");
+const dbconnection = require('../config/db_connection');
+console.log('hi');
 const getCohorts = (city) => {
   console.log("I'm inside the query");
   const sql = {
     text:
-      "select cohorts.name, cohorts.id, cohorts.city,image from projects inner join cohorts on cohorts.id=projects.id where city = $1 ;",
+      'select p.id project_id, c.id cohort_id,p.image project_image,c.name cohort_name from cohorts c,projects p where city=$1;',
     value: [city],
   };
 
   return dbconnection
     .query(sql.text, sql.value)
     .then((res) => res.rows)
-    .catch((err) => console.log("this is an  error"));
+    .catch((err) => console.log('this is an  error'));
 };
 
 module.exports = { getCohorts };
