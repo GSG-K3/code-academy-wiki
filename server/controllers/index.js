e09e5270b06cec27ca16a2832af9a9f6927e3cfa
bayan-404 <bayan.jubah@gmail.com>
2020-05-13 00:45:59 +0300

    add /api/cohorts/projects/:city path

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -4,12 +4,14 @@ const { getCohortData } = require('./getCohortData');
 const { getStudent } = require('./student');
 const { getProject } = require('./getProject');
 const { getAllStudents } = require('./allStudents');
+const { cohortsByCity} = require('./getCohortsByCity');
 
 router.get('/api/cohortinfo/:cohortID', getCohortData);
 router.get('/api/students', getAllStudents);
 router.get('/api/students/:id', getStudent);
 router.get('/api/project/:id', getProject);
-router.get('/api/cohorts/:city', cohorts);
+router.get('/api/cohorts/:city', cohortsByCity);
+router.get('/api/cohorts/projects/:city', cohorts);
 
 module.exports = router;
 
