1aa787b87b7117d5f542a868d512de508603d785
bayan-404 <bayan.jubah@gmail.com>
2020-05-13 00:42:28 +0300

    change the axious to get the data #117

--- client/src/LayoutComponents/Cohorts/Cohorts.js
+++ client/src/LayoutComponents/Cohorts/Cohorts.js
@@ -9,40 +9,55 @@ class Cohorts extends Component {
     super(props);
     this.state = {
       cohortInfo: [],
+      cohortsProjects: [],
     };
   }
   componentDidMount() {
     const cityName = this.props.match.params.city;
+
+    const requestOne = axios.get(`/api/cohorts/${cityName}`);
+    const requestTwo = axios.get(`/api/cohorts/projects/${cityName}`);
+
     axios
-      .get(`/api/cohorts/${cityName}`)
-      .then((res) => {
-        const resData = res.data;
-        this.setState({ cohortInfo: resData });
-        console.log(this.state);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+      .all([requestOne, requestTwo])
+      .then(
+        axios.spread((...responses) => {
+          console.log(responses[0].data,'name quere')
+          console.log(responses[1].data,'other info quere')
+          this.setState({
+            cohortInfo: responses[0].data.reverse(),
+            cohortsProjects: responses[1].data,
+          });
+        })
+      )
+      .catch((err) => err);
   }
+
   render() {
     const cityName = this.props.match.params.city;
-    const { cohortInfo } = this.state;
-        
+    const { cohortInfo, cohortsProjects } = this.state;
+
+    const cohortList = cohortInfo.map((cohort) => {
+      const projectsSlide = cohortsProjects.filter((project) => {
+        return project.cohort_id === cohort.cohort_id;
+      });
+
+      return (
+        <div className = "cohort">
+        <div className = 'coh_card'>
+          <CohortCard  name={cohort.cohort_name} />
+        </div>
+          <div className = 'coh_slide_show'>
+          <Slideshow  projects={projectsSlide}/>
+          </div>
+        </div>
+      );
+    });
+
     return (
-      
       <div>
-      {!cohortInfo.length? <h1>loading</h1>: 
-      <div>
-        <h1 className='title'>{cityName} Cohorts</h1>
-          {cohortInfo.map((e) =>(
-        <div className='cohort'>
-          <div className='coh_card'>
-            <CohortCard name={e.name} /> 
-        </div>
-          <Slideshow projects = {e.image} />
-        </div>
-          ))}
-      </div> }
+      <h1 className = 'city_title'>{`cohort ${cityName}`}</h1>
+    <div>{cohortList}</div>
       </div>
     );
   }
