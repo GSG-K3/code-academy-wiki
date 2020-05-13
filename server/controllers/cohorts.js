deaaddac304ab26156209d9b97e4d7c9c6cc124e
bayan-404 <bayan.jubah@gmail.com>
2020-05-13 22:19:19 +0300

    remove console.log)

--- server/controllers/cohorts.js
+++ server/controllers/cohorts.js
@@ -1,9 +1,7 @@
-const { getCohorts } = require("../database/queries/getCohorts");
-console.log("hi,Im controller");
+const { getCohorts } = require('../database/queries/getCohorts');
 
 exports.cohorts = (req, res) => {
   getCohorts(req.params.city)
     .then((data) => res.json(data))
-    .catch(() => res.status(500).json({ err: "error for get catogery" }));
+    .catch(() => res.status(500).json({ err: 'error for get cohort' }));
 };
-
--- server/controllers/getCohortsByCity.js
+++ server/controllers/getCohortsByCity.js
@@ -1,8 +1,7 @@
-const { getCohortsByCity } = require("../database/queries/getCohortByCity");
+const { getCohortsByCity } = require('../database/queries/getCohortByCity');
 
 exports.cohortsByCity = (req, res) => {
   getCohortsByCity(req.params.city)
     .then((data) => res.json(data))
-    .catch(() => res.status(500).json({ err: "error for get catogery" }));
+    .catch(() => res.status(500).json({ err: 'error for get cohorts' }));
 };
-
