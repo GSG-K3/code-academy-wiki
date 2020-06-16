0dd2590290ecc120b6dbc650db5403ec23856516
rahaf-96 <rahaf.hawamdeh96@gmail.com>
2020-06-16 14:29:16 +0300

    sign up validation using joi #160 #167 #168

--- server/controllers/postUser.js
+++ server/controllers/postUser.js
@@ -1,16 +1,44 @@
 const postUserQuery = require("../database/queries/addUsers");
-exports.postUser = (req, res) => {
-   try{
-  const userInfo = { 
-    username: req.body.username, 
-    email: req.body.email,
-    password: req.body.password,
-  };
-  postUserQuery(userInfo);
-  res.status(200).json({ message: "user  created succefully" }); 
-}
- catch(err){
-      res.status(400).json({message:"There is no valid information"})
-  }
 
+const joi = require("@hapi/joi");
+const validationObject = {
+  username: joi
+    .string()
+    .min(2)
+    .max(40),
+  email: joi
+    .string()
+    .email({
+      minDomainSegments: 2,
+      tlds: { allow: ["com"] },
+    })
+    .min(10),
+  password: joi
+    .string()
+    .alphanum()
+    .min(6)
+    .pattern(new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])")),
+  verifiedPassword: joi.ref("password"),
+};
+
+const schema = joi.object(validationObject);
+
+exports.postUser = (req, res) => {
+  const { error, value } = schema.validate(req.body);
+  const { username, email, password } = value;
+  if (error) {
+    res.status(400).json({ error: error.details[0].message });
+  } else {
+    try {
+      const userInfo = {
+        username,
+        email,
+        password,
+      };
+      postUserQuery(userInfo);
+      res.status(200).json({ message: "user  created succefully" });
+    } catch (err) {
+      res.status(400).json({ message: "There is no valid information" });
+    }
+  }
 };
