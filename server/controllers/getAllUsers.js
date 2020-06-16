61ba85213e0c8dc045aadc63a88b92e9f4ac018b
kholoudfann <141060@ppu.edu.ps>
2020-06-16 13:07:32 +0300

    delete those two files

--- server/controllers/getAllUsers.js
+++
@@ -1,7 +0,0 @@
-const getAllUsersQuery = require('../database/queries/getAllUsers');
-
-exports.getAllUsers = (req, res, next) => {
-  getAllUsersQuery()
-    .then((data) => res.json(data))
-    .catch((err) => next(err));
-};
