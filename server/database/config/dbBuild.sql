4dcd90713d3ad62b68aeff8dcb46dc9544f2f50e
kholoudfann <141060@ppu.edu.ps>
2020-06-16 13:38:14 +0300

    update on database

--- server/database/config/dbBuild.sql
+++ server/database/config/dbBuild.sql
@@ -66,8 +66,8 @@ CREATE TABLE std_projects
 CREATE TABLE users
 (
   id SERIAL PRIMARY KEY,
-  username VARCHAR(50) NOT NULL,
-  email VARCHAR(255) NOT NULL,
+  username VARCHAR(50) NOT NULL UNIQUE,
+  email VARCHAR(255) NOT NULL UNIQUE,
   password VARCHAR(255) NOT NULL,
   type VARCHAR(50)
   );
