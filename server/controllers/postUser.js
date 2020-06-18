f115b56058149b65c8328917a05b656aa6f22d63
rahaf-96 <rahaf.hawamdeh96@gmail.com>
2020-06-18 11:17:54 +0300

    edit the maximum number of characters to each of username, email and password also add repassword to validation schema

--- server/controllers/postUser.js
+++ server/controllers/postUser.js
@@ -5,30 +5,33 @@ const validationObject = {
   username: joi // username should be string and at least it contains 2 characters
     .string()
     .min(2)
-    .max(40),
+    .max(50),
   email: joi // email should be string and valid and its tld should be .com
     .string()
     .email({
       minDomainSegments: 2,
       tlds: { allow: ["com"] },
     })
-    .min(10),
+    .min(10)
+    .max(255),
   password: joi // password must be string, at least have 6 characters contains capital letters, small letter and numbers
     .string()
     .alphanum()
     .min(6)
+    .max(255)
     .pattern(new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])")),
+  repassword: joi.ref("password"),
 };
 
 const schema = joi.object(validationObject);
 
 exports.postUser = (req, res) => {
   const { error, value } = schema.validate(req.body);
-  const { username, email, password } = value;
   if (error) {
     res.status(400).json({ error: error.details[0].message });
   } else {
     try {
+      const { username, email, password } = value;
       const userInfo = {
         username,
         email,
