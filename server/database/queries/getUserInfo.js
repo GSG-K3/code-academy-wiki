cb1c21e8ab30e3c5b8c06ad4994483d384a3876d
Hussein-shahatet97 <hussein.sh1997@hotmail.com>
2020-06-18 11:33:26 +0300

    query to git all information of user

---
+++ server/database/queries/getUserInfo.js
@@ -0,0 +1,13 @@
+const dbconnection = require('../config/dbConnection');
+const getUserInfo = (id) => {
+  const sql = {
+    text: 'select * from users where  id = $1;',
+    values: [id],
+  };
+  return dbconnection
+    .query(sql.text, sql.values)
+    .then((res) => res.rows)
+    .catch((err) => err);
+};
+
+module.exports = getUserInfo;
