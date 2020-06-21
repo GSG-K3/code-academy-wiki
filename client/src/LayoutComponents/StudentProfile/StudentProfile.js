e53367be97d8449d9a1e19c9187236bd6543eb7b
kholoudfann <141060@ppu.edu.ps>
2020-06-21 10:42:18 +0300

    solve eslint problem

--- client/src/LayoutComponents/StudentProfile/StudentProfile.js
+++ client/src/LayoutComponents/StudentProfile/StudentProfile.js
@@ -1,14 +1,14 @@
-import React, { Component } from "react";
-import axios from "axios";
-import email from "../../images/contact-icons/email.svg";
-import github from "../../images/contact-icons/github.svg";
-import phone from "../../images/contact-icons/phone.svg";
-import house from "../../images/contact-icons/house.svg";
-import Project from "../../SharedComponents/ProjectView";
-import { Link } from "react-router-dom";
-import "./StudentProfile.css";
-import Loading from "../../SharedComponents/Loading";
-import PageNotFound from "../PageNotFound";
+import React, { Component } from 'react';
+import axios from 'axios';
+import email from '../../images/contact-icons/email.svg';
+import github from '../../images/contact-icons/github.svg';
+import phone from '../../images/contact-icons/phone.svg';
+import house from '../../images/contact-icons/house.svg';
+import Project from '../../SharedComponents/ProjectView';
+import { Link } from 'react-router-dom';
+import './StudentProfile.css';
+import Loading from '../../SharedComponents/Loading';
+import PageNotFound from '../PageNotFound';
 
 class StudentProfile extends Component {
   state = {
@@ -59,48 +59,48 @@ class StudentProfile extends Component {
           {!studentInfo ? (
             <Loading />
           ) : (
-            <div className="profile-container">
-              <div className="student-card">
+            <div className='profile-container'>
+              <div className='student-card'>
                 {studentInfo.image ? (
-                  <img className="student-img" src={studentInfo.image} alt="" />
+                  <img className='student-img' src={studentInfo.image} alt='' />
                 ) : (
-                  <div className="student-img">
+                  <div className='student-img'>
                     {studentInfo.name.charAt(0)}
                   </div>
                 )}
 
-                <span className="student-card-name">{studentInfo.name} </span>
-                <span className="student-role"> {studentInfo.role} </span>
-                <div className="contact-section">
-                  <div className="contact-info">
-                    <img className="contact-icon" src={email} alt="" />
+                <span className='student-card-name'>{studentInfo.name} </span>
+                <span className='student-role'> {studentInfo.role} </span>
+                <div className='contact-section'>
+                  <div className='contact-info'>
+                    <img className='contact-icon' src={email} alt='' />
                     <span>{studentInfo.email}</span>
                   </div>
-                  <div className="contact-info">
-                    <img className="contact-icon" src={github} alt="" />
+                  <div className='contact-info'>
+                    <img className='contact-icon' src={github} alt='' />
                     <span>
                       <a
                         href={studentInfo.github}
-                        target="_blank"
-                        rel="noopener noreferrer"
+                        target='_blank'
+                        rel='noopener noreferrer'
                       >
                         {studentInfo.name} github
                       </a>
                     </span>
                   </div>
-                  <div className="contact-info">
-                    <img className="contact-icon" src={phone} alt="" />
+                  <div className='contact-info'>
+                    <img className='contact-icon' src={phone} alt='' />
                     <span>{studentInfo.phone_no}</span>
                   </div>
-                  <div className="contact-info">
-                    <img className="contact-icon" src={house} alt="" />
+                  <div className='contact-info'>
+                    <img className='contact-icon' src={house} alt='' />
                     <span>{studentInfo.address}</span>
                   </div>
                 </div>
               </div>
-              <div className="student-projects-container">
-                <p className="projects-title">{studentInfo.name} Projects</p>
-                <div className="student-projects"> {projects}</div>
+              <div className='student-projects-container'>
+                <p className='projects-title'>{studentInfo.name} Projects</p>
+                <div className='student-projects'> {projects}</div>
               </div>
             </div>
           )}
