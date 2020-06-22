253575b761f03d0f7484a373b9dd53a7d0a43af5
Hussein-shahatet97 <hussein.sh1997@hotmail.com>
2020-06-22 15:09:02 +0300

    Create form and functions that communicate with back-end login

--- client/src/LayoutComponents/Login/login.js
+++ client/src/LayoutComponents/Login/login.js
@@ -1,38 +1,64 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './login.css';
 
 class login extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: '',
-      password: '',
-    };
-  }
+  state = {
+    email: '',
+    password: '',
+    errorMsg: '',
+    login: false,
+  };
+
+  changName = (event) => {
+    const { target } = event;
+    const { value, name } = target;
+    this.setState({ [name]: value });
+  };
+
+  goLogedin = (event) => {
+    event.preventDefault();
+    const { email, password } = this.state;
+    const { page } = this.props;
+
+    axios
+      .post('/api/login', { email, password })
+      .then((res) => console.log(res.data.msg))
+      .catch((err) => console.log(err.response.data));
+  };
+
 
   render() {
+    const { email, password, errorMsg } = this.state;
     return (
       <div>
-        <div className='form'>
-          <input
-            className='FormInput'
-            type='text'
-            name='username'
-            placeholder='Enter your user name'
-            required
-          />
-          <input
-            className='FormInput'
-            type='password'
-            name='password'
-            placeholder='Enter your password'
-            required
-          />
-
-          <button name='submit' type='submit' className='login-button'>
-            Login
-          </button>
-        </div>
+        <form onSubmit={this.goLogedin}>
+          <div className='form'>
+            <h1 className='form-title'>Login as admin</h1>
+            <input
+              className='FormInput'
+              type='email'
+              name='email'
+              placeholder='Enter your email'
+              value={email}
+              onChange={this.changName}
+              required
+            />
+            <input
+              className='FormInput'
+              value={password}
+              type='password'
+              name='password'
+              placeholder='Enter your password'
+              onChange={this.changName}
+              required
+            />
+            <button className='login-button' type='submit'>
+              login
+            </button>
+            {errorMsg && <p className='invalidMsg'>{errorMsg}</p>}
+          </div>
+        </form>
       </div>
     );
   }
