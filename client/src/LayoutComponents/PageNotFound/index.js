faa4643fda12a55e03039509a24ac297418b087b
bayan-404 <bayan.jubah@gmail.com>
2020-06-24 17:39:06 +0300

    fix the litters

---
+++ client/src/LayoutComponents/PageNotFound/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import image from '../../images/error.png';
+const ErrorImage = () => {
+  return (
+    <div className='error-page'>
+      <img src={image} alt='image' />;
+    </div>
+  );
+};
+export default ErrorImage;
